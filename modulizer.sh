284701db444f58aa41380a1c4e0be96160d5c0fb
Paul Varache <perso@paulvarache.ninja>
2018-05-26 13:21:42 +0100

    Bump version

--- modulizer.sh
+++ modulizer.sh
@@ -1,1 +1,1 @@
-modulizer --out . --npm-name @kano/kwc-icons --npm-version 3.0.0-beta.1 --dependency-mapping kwc-style,@kano/kwc-style,^3.0.0-beta.1
+modulizer --out . --npm-name @kano/kwc-icons --npm-version 3.0.0-beta.2 --dependency-mapping kwc-style,@kano/kwc-style,^3.0.0-beta.2 --import-style name
